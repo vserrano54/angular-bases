3538e001600fa7808104285b5387e9cf44ad8e45
vserrano54 <vserrano54yahoo.com>
2023-10-09 19:24:52 +0200

    Fin de seccion 4

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CounterModule } from './counter/counter.modules';
+import { HeroesModule } from './heroes/heroes.modules';
+
 import { AppComponent } from './app.component';
+
+
 
 @NgModule({
   declarations: [
     AppComponent
+  
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CounterModule,
+    HeroesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
